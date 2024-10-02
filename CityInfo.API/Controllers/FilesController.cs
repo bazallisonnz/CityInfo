a9d97d1fe439940edcee5b8ef90042f3309cab87
Barry Allison <bazallison@hotmail.com>
2024-10-02 17:05:58 +1300

    Returning resources, routing, model (DTO) classes, using the right status codes, returning child resources, using ProblemDetails, content negotiation, downloading a file.

---
+++ CityInfo.API/Controllers/FilesController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.StaticFiles;
+
+namespace CityInfo.API.Controllers;
+
+[Route("api/files")]
+[ApiController]
+public class FilesController : ControllerBase
+{
+    private readonly FileExtensionContentTypeProvider _fileExtensionContentTypeProvider;
+
+    public FilesController(FileExtensionContentTypeProvider fileExtensionContentTypeProvider)
+    {
+        _fileExtensionContentTypeProvider = fileExtensionContentTypeProvider
+                                            ?? throw new ArgumentNullException(nameof(fileExtensionContentTypeProvider));
+    }
+
+    [HttpGet("{fileId}")]
+    public ActionResult GetFile(int fileId)
+    {
+        // look up the actual file, depending on the fileId...
+        // demo code
+        var pathToFile = "NA-140VG4_NA-148VG4_Operating_Instructions.pdf";
+
+        // Check if the file exists.
+        if (!System.IO.File.Exists(pathToFile))
+        {
+            return NotFound();
+        }
+
+        if (!_fileExtensionContentTypeProvider.TryGetContentType(pathToFile, out var contentType))
+        {
+            contentType = "application/octet-stream";
+        }
+
+        var bytes = System.IO.File.ReadAllBytes(pathToFile);
+        return File(bytes, contentType, Path.GetFileName(pathToFile));
+    }
+}
--- CityInfo.API/Program.cs
+++ CityInfo.API/Program.cs
@@ -1,11 +1,28 @@
+using Microsoft.AspNetCore.StaticFiles;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddControllers(options =>
+{
+    // This will return a 406 Not Acceptable if the client requests a format that the server does not support.
+    options.ReturnHttpNotAcceptable = true;
+}).AddXmlDataContractSerializerFormatters();
 
-builder.Services.AddControllers();
+// Can add extra useful values to the ProblemDetails object.
+// Not useful for this simple case, but a good reminder of what can be done.
+builder.Services.AddProblemDetails(options =>
+{
+    options.CustomizeProblemDetails = ctx =>
+    {
+        ctx.ProblemDetails.Extensions.Add("server", Environment.MachineName);
+    };
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<FileExtensionContentTypeProvider>();
 
 var app = builder.Build();
 
@@ -18,7 +35,14 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseRouting();
+
 app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
 
 app.MapControllers();
 
